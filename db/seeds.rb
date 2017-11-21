ba237b7373d47784525cddf84d741c38610bac91
Polo Duf <paulhenri.dufourcq@gmail.com>
2017-11-21 17:14:40 +0100

    Rail wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 10.times do
-  title     = Faker::File.extension
+  title     = Faker::File.extension.unique
   content   = Faker::Lorem.sentence
 
   Article.create(title: title, content: content)
 end
+
+
+Kramdown::Document.new(text).to_html
